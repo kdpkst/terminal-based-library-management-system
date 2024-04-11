a580f3f942a58f2fd29914460410a7363a76da01
kdpkst <jammiechen0@gmail.com>
2024-04-11 14:24:28 +0800

    completed initial version of factory method for books

--- Models/BookFactory/bookFactory.java
+++ Models/BookFactory/bookFactory.java
@@ -3,5 +3,26 @@ package Models.BookFactory;
 import Models.Book.book;
 
 public abstract class bookFactory {
-    public abstract book createBook(int bid, String title, String author, String genre, int quantity_available);
+
+    public abstract book createSpecificBook(int bid, String title, String author, String genre, int quantity_available);
+
+    public book createBook(int bid, String title, String author, String genre, int quantity_available){
+        switch (genre) {
+            case "science":
+                scienceBookFactory sBookFactory = new scienceBookFactory();
+                sBookFactory.createBook(bid, title, author, genre, quantity_available);
+            case "novel":
+                novelBookFactory nBookFactory = new novelBookFactory();
+                nBookFactory.createBook(bid, title, author, genre, quantity_available);
+            case "business":
+                businessBookFactory bBokkBookFactory = new businessBookFactory();
+                bBokkBookFactory.createBook(bid, title, author, genre, quantity_available);
+            case "history":
+                historyBookFactory hBookFactory = new historyBookFactory();
+                hBookFactory.createBook(bid, title, author, genre, quantity_available);
+            default:
+                return null;
+        }
+    }
+
 }
